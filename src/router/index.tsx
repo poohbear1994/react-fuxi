d7dc2f4439e84a233907ca246add30f965023b8b
poohbear <452363993@qq.com>
2023-04-26 22:13:17 +0800

    feat: 开发home页

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -75,7 +75,7 @@ const router = createBrowserRouter([
 
 export default router
 
-export const HOME_PATHNAME = '/home'
+export const HOME_PATHNAME = '/'
 export const LOGIN_PATHNAME = '/login'
 export const REGISTER_PATHNAME = '/register'
 export const MANAGE_INDEX_PATHNAME = '/manage/list'
