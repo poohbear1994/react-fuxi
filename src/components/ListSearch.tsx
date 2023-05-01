6ee0b0c0ca42d0dc130daf151d57adea30e1825b
poohbear <452363993@qq.com>
2023-05-01 15:50:31 +0800

    feat: 搜索组件功能完成

--- src/components/ListSearch.tsx
+++ src/components/ListSearch.tsx
@@ -1,18 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import type { ChangeEvent, FC } from 'react'
 import { Input } from 'antd'
+import { useNavigate, useLocation, useSearchParams } from 'react-router-dom'
+import { LIST_SEARCH_PARAM_KEY } from '../constant/index'
 
 const { Search } = Input
 
 const ListSearch: FC = () => {
 	const [value, setValue] = useState('')
+	const { pathname } = useLocation()
+	const nav = useNavigate()
+	const [searchParams] = useSearchParams()
+
+	useEffect(() => {
+		// 每当searchParams有变化，都会执行该回调
+		const curVal = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
+		setValue(curVal)
+	}, [searchParams])
 
 	const handleSearch = (value: string) => {
-		console.info(value)
+		// 跳转页面，增加url参数
+		nav({
+			pathname,
+			search: `${LIST_SEARCH_PARAM_KEY}=${value}`,
+		})
 	}
 
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setValue(e.target.value)
+		console.info(value)
 	}
 
 	return (
