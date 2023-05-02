aac0da2563a90cb972e7ce5c755ef1ccd83af0b6
poohbear <452363993@qq.com>
2023-05-02 16:16:22 +0800

    feat: 使用loading防止多次触发点击行为，多次触发请求

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,20 +1,42 @@
-import React, { FC } from 'react'
-import { Button, Space, Divider } from 'antd'
+import React, { useState } from 'react'
+import type { FC } from 'react'
+import { Button, Space, Divider, message } from 'antd'
 import { PlusOutlined, BarsOutlined, StarOutlined, DeleteOutlined } from '@ant-design/icons'
-import styles from './ManageLayout.module.scss'
 import { Outlet, useNavigate, useLocation } from 'react-router-dom'
+import styles from './ManageLayout.module.scss'
+import { createQuestionService } from '../services/question'
 
 const ManageLayout: FC = () => {
 	const nav = useNavigate()
 	// 获取路由path
 	const { pathname } = useLocation()
 
+	const [loading, setLoading] = useState(false)
+	const handleCreateClick = async () => {
+		setLoading(true)
+
+		const data = await createQuestionService()
+		const { id } = data || {}
+		if (id) {
+			nav(`/question/edit/${id}`)
+			message.success('创建成功')
+		}
+
+		setLoading(false)
+	}
+
 	return (
 		<>
 			<div className={styles.container}>
 				<div className={styles.left}>
 					<Space direction="vertical">
-						<Button type="primary" size="large" icon={<PlusOutlined />}>
+						<Button
+							type="primary"
+							size="large"
+							icon={<PlusOutlined />}
+							onClick={handleCreateClick}
+							loading={loading}
+						>
 							创建问卷
 						</Button>
 						<Divider style={{ borderTop: 'transparent' }} />
--- src/services/question.ts
+++ src/services/question.ts
@@ -1,8 +1,18 @@
 import axios from './ajax'
 import type { ResDataType } from './ajax'
 
+/**
+ * @description: 获取单个问卷信息
+ * @param {string} id 问卷id
+ */
 export const getQuestionService = async (id: string): Promise<ResDataType> => {
 	const url = `/api/question/${id}`
 	const data = (await axios.get(url)) as ResDataType
+	return data
+}
+
+export const createQuestionService = async (): Promise<ResDataType> => {
+	const url = '/api/question'
+	const data = (await axios.post(url)) as ResDataType
 	return data
 }
