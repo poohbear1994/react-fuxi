130d4d0888b100bd8487c2c51702f64b2ffbe5ee
poohbear <452363993@qq.com>
2023-05-06 21:57:28 +0800

    style: 编辑器整体布局

--- src/layouts/QuestionLayout.tsx
+++ src/layouts/QuestionLayout.tsx
@@ -5,22 +5,21 @@ import useLoadUserData from '../hooks/useLoadUserData'
 import useNavPage from '../hooks/useNavPage'
 
 const QuestionLayout: FC = () => {
+	// 加载用户信息
 	const { waitingUserData } = useLoadUserData()
+	// 未登录时，跳转到登录页
 	useNavPage(waitingUserData)
 
 	return (
-		<>
-			<div>QuestionLayout header</div>
-			<div>
-				{waitingUserData ? (
-					<div style={{ textAlign: 'center', marginTop: '60px' }}>
-						<Spin />
-					</div>
-				) : (
-					<Outlet />
-				)}
-			</div>
-		</>
+		<div style={{ height: '100vh' }}>
+			{waitingUserData ? (
+				<div style={{ textAlign: 'center', marginTop: '60px' }}>
+					<Spin />
+				</div>
+			) : (
+				<Outlet />
+			)}
+		</div>
 	)
 }
 
--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import type { FC } from 'react'
 import useLoadQuestionData from '../../../hooks/useLoadQuestionData'
+import styles from './index.module.scss'
 
 const Edit: FC = () => {
 	const { loading, data: questionData } = useLoadQuestionData()
 	return (
-		<div>
-			<div>Edit Page</div>
-			{loading ? <p>loading</p> : <p>{JSON.stringify(questionData)}</p>}
+		<div className={styles.container}>
+			{/* 编辑器顶部 */}
+			<div style={{ backgroundColor: 'yellowgreen' }}>Header</div>
+			{/* 编辑器主体 */}
+			<div className={styles['content-wrapper']}>
+				<div className={styles.content}>
+					<div className={styles.left}>Left</div>
+					<div className={styles.main}>
+						<div className={styles['canvas-wrapper']}>
+							<div style={{ height: '900px' }}>画布，测试滚动</div>
+						</div>
+					</div>
+					<div className={styles.right}>right</div>
+				</div>
+			</div>
 		</div>
 	)
 }
