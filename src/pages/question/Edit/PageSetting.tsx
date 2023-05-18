823ecc7ce5234c4bbf39d5438c15ae28f767429f
poohbear <452363993@qq.com>
2023-05-18 22:33:23 +0800

    feat: 动态切换右边面板tab

---
+++ src/pages/question/Edit/PageSetting.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import type { FC } from 'react'
+
+const PageSetting: FC = () => {
+	return <div>PageSetting</div>
+}
+
+export default PageSetting
--- src/pages/question/Edit/RightPanel.tsx
+++ src/pages/question/Edit/RightPanel.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import type { FC } from 'react'
 import { Tabs } from 'antd'
 import type { TabsProps } from 'antd'
 import { FileTextOutlined, SettingOutlined } from '@ant-design/icons'
 import ComponentProp from './ComponentProp'
+import PageSetting from './PageSetting'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
+
+enum TAB_KEYS {
+	PROP_KEY = 'prop',
+	SETTING_KEY = 'setting',
+}
 
 const RightPanel: FC = () => {
+	const [activeKey, setActiveKey] = useState(TAB_KEYS.PROP_KEY)
+
+	const { selectedId } = useGetComponentInfo()
+
+	useEffect(() => {
+		if (selectedId) {
+			setActiveKey(TAB_KEYS.PROP_KEY)
+		} else {
+			setActiveKey(TAB_KEYS.SETTING_KEY)
+		}
+	}, [selectedId])
+
 	const tabsItems: TabsProps['items'] = [
 		{
-			key: 'prop',
+			key: TAB_KEYS.PROP_KEY,
 			label: (
 				<span>
 					<FileTextOutlined />
@@ -18,18 +37,18 @@ const RightPanel: FC = () => {
 			children: <ComponentProp />,
 		},
 		{
-			key: 'layers',
+			key: TAB_KEYS.SETTING_KEY,
 			label: (
 				<span>
 					<SettingOutlined />
 					页面设置
 				</span>
 			),
-			children: <div>页面设置</div>,
+			children: <PageSetting />,
 		},
 	]
 
-	return <Tabs defaultActiveKey="componentLib" items={tabsItems} />
+	return <Tabs activeKey={activeKey} items={tabsItems} />
 }
 
 export default RightPanel
