69a57da418eb9aa9758b8a721d841bd1976ec5ce
poohbear <452363993@qq.com>
2023-06-01 22:20:47 +0800

    feat: stat统计列表

--- src/pages/question/Stat/PageStat.tsx
+++ src/pages/question/Stat/PageStat.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import type { FC } from 'react'
-import { Spin, Typography } from 'antd'
+import { Spin, Typography, Table, Pagination } from 'antd'
 import { useRequest } from 'ahooks'
 import { useParams } from 'react-router-dom'
 import { getQuestionStatListService } from '../../../services/stat'
+import useGetComponentInfo from '../../../hooks/useGetComponentInfo'
 
 type PropsType = {
 	selectedComponentId: string
@@ -14,29 +15,73 @@ type PropsType = {
 const { Title } = Typography
 
 const PageStat: FC<PropsType> = props => {
-	const { selectedComponentId } = props
+	const { selectedComponentId, setSelectedComponentId, setSelectedComponentType } = props
 
 	const { id: questionId = '' } = useParams()
 
+	const { componentList } = useGetComponentInfo()
+
+	const columns = componentList.map(c => {
+		const { title, fe_id, props, type } = c
+		const columnTitle = props!.title || title
+
+		return {
+			title: (
+				<div
+					style={{ cursor: 'pointer' }}
+					onClick={() => {
+						setSelectedComponentId(fe_id)
+						setSelectedComponentType(type)
+					}}
+				>
+					{fe_id === selectedComponentId && <span style={{ color: '#1890ff' }}>{columnTitle}</span>}
+					{fe_id !== selectedComponentId && <span>{columnTitle}</span>}
+				</div>
+			),
+			dataIndex: fe_id,
+		}
+	})
+
+	const [page, setPage] = useState(1)
+	const [pageSize, setPageSize] = useState(10)
 	const [total, setTotal] = useState(0)
-	const [list, setList] = useState([])
+	const [list = [], setList] = useState<Array<any>>([])
 
 	const { loading } = useRequest(
 		async () => {
 			const params = {
-				page: 1,
-				pageSize: 10,
+				page,
+				pageSize,
 			}
 			const data = await getQuestionStatListService(questionId, params)
 			return data
 		},
 		{
+			refreshDeps: [page, pageSize, questionId], // 当依赖变化时，自动发起请求
 			onSuccess(result) {
 				const { total, list = [] } = result
 				setTotal(total)
 				setList(list)
 			},
 		}
+	)
+
+	const TableElem = (
+		<>
+			<Table columns={columns} dataSource={list} pagination={false} rowKey={q => q._id} />
+			<div style={{ textAlign: 'center', marginTop: '18px' }}>
+				<Pagination
+					total={total}
+					pageSize={pageSize}
+					current={page}
+					onChange={page => setPage(page)}
+					onShowSizeChange={(page, pageSize) => {
+						setPage(page)
+						setPageSize(pageSize)
+					}}
+				/>
+			</div>
+		</>
 	)
 
 	return (
@@ -47,6 +92,7 @@ const PageStat: FC<PropsType> = props => {
 					<Spin />
 				</div>
 			)}
+			{!loading && TableElem}
 		</div>
 	)
 }
