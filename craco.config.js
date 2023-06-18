fa77f470d60a20765b4e31b121f09fdc8b8cdf8c
poohbear <452363993@qq.com>
2023-06-18 16:29:44 +0800

    perf: 抽离公共代码antd和react，合理使用HTTP缓存

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,40 @@
 /* eslint-disable */
 module.exports = {
+	webpack: {
+		// 这里可以获得项目的webpack的配置，可以在这对webpack的配置进行修改
+		configure(webpackConfig) {
+			if (webpackConfig.mode === 'production') {
+				// 抽离公共代码，只在生产环境
+				if (webpackConfig.optimization == null) {
+					webpackConfig.optimization = {}
+				}
+				webpackConfig.optimization.splitChunks = {
+					chunks: 'all',
+					cacheGroups: {
+						antd: {
+							name: 'antd-chunk',
+							// 所有import ... from 'antd/*'的都使用该规则
+							test: /antd/,
+							// 优先级
+							priority: 100,
+						},
+						reactDom: {
+							name: 'reactDom-chunk',
+							test: /react-dom/,
+							priority: 99
+						},
+						// 第三方包
+						vendors: {
+							name: 'vendors-chunk',
+							test: /node_modules/,
+							priority: 98
+						}
+					}
+				}
+			}
+			return webpackConfig
+		}
+	},
 	devServer: {
 		port: 8000,
 		proxy: {
